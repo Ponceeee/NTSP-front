937b2f8c12300f1e2880ebac1909597b3cc77f94
Ian Dave L. Javier <2201105150@student.buksu.edu.ph>
2024-11-16 21:58:11 +0800

    ..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,15 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
+import UserDashboard from './components/user/UserDashboard';
+import ProtectedRoute from './components/ProtectedRoute';
+import AdminDashboard from './components/AdminDashboard';
 // import Sidebar from './components/Sidebar';
 // import AdminDashboard from './components/UserDashboard';
 // import RequestPage from './components/RequestPage';
 // import EditPage from './components/EditPage';
 // import NotificationPage from './components/NotificationPage';
 // import ReportsPage from './components/ReportsPage';
-import UserDashboard from './components/user/UserDashboard';
-import ProtectedRoute from './components/ProtectedRoute';
-import AdminDashboard from './components/AdminDashboard';
 // import RequestPage from './components/RequestPage';
 
 function App() {
@@ -19,11 +19,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
-        {/* <Route path="/admin" element={<ProtectedRoute role="admin"><AdminDashboard /></ProtectedRoute>} /> */}
-        <Route path="/request" element={<RequestPage />} />
-        {/* <Route path="/edit" element={<Editpage/>} /> */}
-        {/* <Route path="/reports" element={<ReportsPage/>} /> */}
-        {/* <Route path="/user" element={<ProtectedRoute role="user"><UserDashboard /></ProtectedRoute>} /> */}
+        <Route path="/admin" element={<ProtectedRoute role="admin"><AdminDashboard /></ProtectedRoute>} />
+        {/* <Route path="/requestpage" element={<RequestPage />} /> */}
+        <Route path="/user" element={<ProtectedRoute role="user"><UserDashboard /></ProtectedRoute>} />
         {/* <Route path="/request" element={<ProtectedRoute role="user"><Sidebar /></ProtectedRoute>} /> */}
       </Routes>
     </>
